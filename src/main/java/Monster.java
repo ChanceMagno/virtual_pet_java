148f57dca7a78123efa505c6db128c64d2c47813
Chance Magno and Taylor Loftis-Kim <chance+taylor@>
2017-04-03 10:38:20 -0700

    add basic class members and test, create database and test database

--- src/main/java/Monster.java
+++ src/main/java/Monster.java
@@ -1,9 +1,73 @@
 import org.sql2o.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Monster {
+  private int personId;
+  private String name;
+  private int id;
+  private int foodLevel;
+  private int sleepLevel;
+  private int playLevel;
 
-  public Monster() {
+  public static final int MAX_FOOD_LEVEL = 3;
+  public static final int MAX_SLEEP_LEVEL = 8;
+  public static final int MAX_PLAY_LEVEL = 12;
+  public static final int MIN_ALL_LEVELS = 0;
 
+  public Monster(String name, int personId) {
+    this.name = name;
+    this.personId = personId;
   }
 
+  public String getName() {
+    return name;
+  }
+
+  public int getPersonId() {
+    return personId;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  @Override
+  public boolean equals(Object otherMonster){
+    if (!(otherMonster instanceof Monster)) {
+      return false;
+    } else {
+      Monster newMonster = (Monster) otherMonster;
+      return this.getName().equals(newMonster.getName()) &&
+             this.getPersonId() == newMonster.getPersonId();
+    }
+  }
+
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO monsters (name, personid) VALUES (:name, :personId)";
+      this.id = (int) con.createQuery(sql, true)
+        .addParameter("name", this.name)
+        .addParameter("personId", this.personId)
+        .executeUpdate()
+        .getKey();
+    }
+  }
+
+  public static List<Monster> all() {
+    String sql = "SELECT * FROM monsters";
+    try(Connection con = DB.sql2o.open()) {
+      return con.createQuery(sql).executeAndFetch(Monster.class);
+    }
+  }
+
+  public static Monster find(int id) {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM monsters where id=:id";
+      Monster monster = con.createQuery(sql)
+        .addParameter("id", id)
+        .executeAndFetchFirst(Monster.class);
+      return monster;
+    }
+  }
 }
